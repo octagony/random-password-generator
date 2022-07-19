2e8ec643a44e936fdc8c8d3bbfd95865c515f0de
Octagony <moctagony@gmail.com>
2022-07-19 20:37:15 +0300

    set a function of the generator operation

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,36 +2,105 @@ import React, { useState, useEffect } from "react";
 const generator = require("generate-password");
 
 const Main = () => {
-  /*  const password = generator.generate({
-    length:10,
-    numbers:true,
-  }) */
+  const [password, setPassword] = useState("");
+  
+  const [passwordLength,setPasswordLength] = useState(2);
 
-  const [password, setPassword] = useState(null);
+  const [initialState, setInitialState] = useState({
+    length: 10,
+    numbers: false,
+    symbols: false,
+    lowercase: true,
+    uppercase: false,
+  });
 
-  useEffect(() => {
-    setPassword(
-      generator.generate({
-        length: 50,
-        numbers: true,
-        symbols: true,
-      })
-    );
-  }, []);
-
-  const handleGenerate = () =>{
-    setPassword(
-      generator.generate({
-        length: 50,
-        numbers: true,
-        symbols: true,
-      })
-    );
+  const handlePasswordLenght = (event) =>{
+    setPasswordLength(event.target.value)
+    setInitialState({
+      ...initialState,
+      [event.target.name]: event.target.value
+    })
   }
+
+  const updateInputs = (event) => {
+    setInitialState({
+      ...initialState,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const updateCheckboxes = (event) => {
+    setInitialState({
+      ...initialState,
+      [event.target.name]: event.target.checked,
+    });
+
+  };
+
+  const handleGeneratePassword = (event) => {
+    event.preventDefault();
+
+
+
+    setInitialState({
+      ...initialState,
+      length: parseInt(initialState.length),
+    });
+
+
+    const password = generator.generate(initialState);
+    setPassword(password);
+  };
 
   return (
     <div className="text-center">
       <h2>Let&apos;s create a password for you!</h2>
+      <div>
+        <form
+          onSubmit={handleGeneratePassword}
+          className="p-4 border-2 border-amber-400"
+        >
+          <div>
+            <span>Your password: </span>
+            <p>{password}</p>
+          </div>
+          <div>
+            <span>Password Length:</span>
+             <input
+              type="range"
+              name="length"
+              onChange={handlePasswordLenght}
+              min='2'
+              max='100'
+              value={passwordLength}
+            /> 
+            <span>{passwordLength}</span>
+          </div>
+          <div>
+            <span>Include numbers:</span>
+            <input type="checkbox" name="numbers" onChange={updateCheckboxes} />
+          </div>
+          <div>
+            <span>Include symbols:</span>
+            <input type="checkbox" name="symbols" onChange={updateCheckboxes} />
+          </div>
+          <div>
+            <span>Include uppercase:</span>
+            <input
+              type="checkbox"
+              name="uppercase"
+              onChange={updateCheckboxes}
+            />
+          </div>
+          <div>
+            <input
+              className="p-2 bg-slate-500 cursor-pointer"
+              type="submit"
+              value="Generate password"
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
