2ea8cdf670e1ae9758e71db9d44cd032bce5d3a2
octagony <moctagony@gmail.com>
2022-10-11 21:01:28 +0300

    [DEVELOP] Added Navbar and Mobile menu

--- components/ThemeToggle.jsx
+++ components/ThemeToggle.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import {HiSun, HiMoon} from "react-icons/hi";
+import React, {useContext} from 'react'
+import { HiSun, HiMoon } from "react-icons/hi";
 import { ThemeContext } from "../context/ThemeContext";
 
 const ThemeToggle = () => {
-	const [theme, setTheme]  = useContext(ThemeContext);	
+	const { theme, setTheme }  = useContext(ThemeContext);	
   return (
-	{theme === 'dark' ? (
-		<div>
-		<HiSun/> Light Mode
+	  <div className = 'p-2'>
+	{ theme === 'dark' ? (
+		<div className = 'flex items-center cursor-pointer' onClick = { () => setTheme(theme === 'dark' ? 'light' : 'dark')} >
+		 <HiSun className = 'text-primary text-2xl mr-2' /> Light Mode
 		</div>
-	): <div>
-		<HiMoon/> Dark Mode	
-	</div> }
+	) : ( <div className = 'flex items-center cursor-pointer' onClick = { () => setTheme(theme === 'dark' ? 'light' : 'dark')} >
+		<HiMoon className= 'text-primary text-2xl mr-2' /> Dark Mode	
+	</div> ) }
+	  </div>
   )
 }
 
