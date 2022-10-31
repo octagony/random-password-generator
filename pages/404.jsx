88399f3c65534b380ed09feb3abe89395a3c4f1b
octagony <moctagony@gmail.com>
2022-10-31 19:21:06 +0300

    [DEVELOP] Make a custom 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+const custom404 = () => {
+  return(
+    <div className="flex flex-col justify-center items-center gap-6">
+      <h2 className="text-5xl font-bold">404</h2>
+      <p className="text-2xl font-bold">Page not found :(</p>
+      <Link href='/'>
+        <a className="cursor-pointer text-btnText p-3 bg-button rounded-2xl shadow-xl">Go back to main page</a>
+      </Link>
+    </div>
+  )
+}
+
+export default custom404
