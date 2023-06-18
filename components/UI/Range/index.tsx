a2140c433f02c1b92d3ea99575dafeddff34e615
octagony <moctagony@gmail.com>
2023-06-18 12:40:38 +0300

    [FIX] Fixed event error in Range component

--- components/UI/Range/index.tsx
+++ components/UI/Range/index.tsx
@@ -1,23 +1,23 @@
-import React from "react";
-import { IRange } from "./Range.props";
-import style from "./Range.module.css";
+import { SyntheticEvent } from 'react'
+import style from './Range.module.css'
+import { IRange } from './Range.props'
 
 const Range = ({ passwordLength, handlePasswordLength }: IRange) => {
-  return (
-    <label className={style.wrapper}>
-      <input
-        type="range"
-        name="length"
-        onChange={() => {
-          handlePasswordLength(event);
-        }}
-        min="10"
-        max="80"
-        value={passwordLength}
-        className={style.input}
-      />
-    </label>
-  );
-};
+	return (
+		<label className={style.wrapper}>
+			<input
+				type='range'
+				name='length'
+				onChange={(event: SyntheticEvent) => {
+					handlePasswordLength(event)
+				}}
+				min='10'
+				max='80'
+				value={passwordLength}
+				className={style.input}
+			/>
+		</label>
+	)
+}
 
-export default Range;
+export default Range
